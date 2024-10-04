22e5d931ffc2fbe22b7aa9f74c0411cc9640f818
Balyeet1 <ricardoldias123@gmail.com>
2024-10-04 11:23:42 +0000

    Added themes different for each pomodoro mode.

--- app/lib/fonts/fonts.ts
+++ app/lib/fonts/fonts.ts
@@ -1,3 +1,5 @@
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 
+
+export const poppins = Poppins({ weight: ["300", "600"], subsets: ["latin"] });
 export const inter = Inter({ weight: ["400", "700"], subsets: ["latin"] });
--- app/pomodoro/page.tsx
+++ app/pomodoro/page.tsx
@@ -3,39 +3,92 @@ import { useState } from 'react';
 import DrawerMenu from '@/app/lib/components/drawerMenu';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import BoltIcon from '@mui/icons-material/Bolt';
+import CoffeeRoundedIcon from '@mui/icons-material/CoffeeRounded';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import PauseIcon from '@mui/icons-material/Pause';
+import RocketLaunchRoundedIcon from '@mui/icons-material/RocketLaunchRounded';
 import FastForwardIcon from '@mui/icons-material/FastForward';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import Button from '@mui/material/Button';
+import { poppins } from '@/app/lib/fonts/fonts';
+import styleVariables from '@/app/pomodoro/variables.module.scss';
 
+type PomodoroMode = {
+  style: string;
+  text: string;
+  Icon: React.ComponentType<any>;
+};
+
+const pomodoroModes: { [key: string]: PomodoroMode } = {
+  focus: {
+    style: styleVariables.focus,
+    text: "Focus",
+    Icon: BoltIcon,
+  },
+  shortBreak: {
+    style: styleVariables.shortBreak,
+    text: "Short Break",
+    Icon: CoffeeRoundedIcon,
+  },
+  longBreak: {
+    style: styleVariables.longBreak,
+    text: "Long Break",
+    Icon: RocketLaunchRoundedIcon,
+  },
+};
 
 export default function HomePage() {
 
-  const [isMenuOpen, setisMenuOpen] = useState(false);
+  const [isMenuOpen, setisMenuOpen] = useState<boolean>(false);
+
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
+  const [currentMode, setCurrentMode] = useState<PomodoroMode>(pomodoroModes.focus);
+
+  const changePomodoroMode = () => {
+    if (currentMode === pomodoroModes.focus) {
+      setCurrentMode(pomodoroModes.shortBreak);
+    } else if (currentMode === pomodoroModes.shortBreak) {
+      setCurrentMode(pomodoroModes.longBreak);
+    } else {
+      setCurrentMode(pomodoroModes.focus);
+    }
+  }
+
+  
 
   return (
     <>
-      <div className="focus-container">
+      <div className={`${currentMode.style}`}>
         <DrawerMenu isOpen={isMenuOpen} onClose={() => setisMenuOpen(false)} />
         <ExpandMoreIcon className='absolute top-4 left-4' onClick={() => setisMenuOpen(true)} />
         <div className='mx-32 p-4 flex flex-col items-center h-screen align-center justify-center'>
           <div className='w-full flex justify-center'>
-            <div className='border-2 flex justify-centen items-center rounded-3xl p-1 mode'>
-              <BoltIcon fontSize="medium"/>
-              <h5 className='text-xl mr-1'>Focus</h5>
+            <div className={`border-2 flex justify-centen items-center rounded-3xl p-1 ${styleVariables.mode}`}>
+              <currentMode.Icon className='mr-1 ml-1' fontSize="medium" />
+              <h5 className='text-xl mr-1'>{currentMode.text}</h5>
             </div>
           </div>
-          <div className='w-full flex flex-col align-center items-center mt-4'>
+          <div className={`w-full flex flex-col align-center items-center mt-4 ${poppins.className}`}>
             <h5 className='text-11xl leading-none'>25</h5>
             <h5 className='text-11xl leading-none'>00</h5>
           </div>
           <div className='flex justify-center align-center items-center flex-nowrap mt-4'>
-            <Button className='second-button'><MoreHorizIcon fontSize="medium"/></Button>
-            <div className='mx-4 flex justify-center'><Button className='primary-button'><PlayArrowIcon fontSize="large"/></Button></div>
-            <Button className='second-button'><FastForwardIcon fontSize="medium" /></Button>
+            <Button className={`${styleVariables.secondButton}`}><MoreHorizIcon fontSize="medium" /></Button>
+            <div className='mx-4 flex justify-center'>
+              <Button
+                onClick={() => setIsTimerRunning(!isTimerRunning)}
+                className={`${styleVariables.primaryButton}`}>
+                {
+                  isTimerRunning
+                    ? <PauseIcon fontSize="large" />
+                    : <PlayArrowIcon fontSize="large" />
+                }
+              </Button>
+            </div>
+            <Button onClick={() => changePomodoroMode()} className={`${styleVariables.secondButton}`}><FastForwardIcon fontSize="medium" /></Button>
           </div>
         </div>
-      </div>
+      </div >
     </>
   );
 }
