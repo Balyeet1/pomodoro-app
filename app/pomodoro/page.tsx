4b2d3bf4aaaeafa52617a80b6457575fc67d8d5e
Balyeet1 <Ricardoldias123@gmail.com>
2024-10-08 14:30:03 +0000

    Refactor pomodoro timer.

--- app/pomodoro/page.tsx
+++ app/pomodoro/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import DrawerMenu from '@/app/lib/components/drawerMenu'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
@@ -9,196 +9,164 @@ import PlayArrowIcon from '@mui/icons-material/PlayArrow'
 import Button from '@mui/material/Button'
 import TimerDisplay from '@/app/lib/components/pomodoro/TimerDisplay'
 import { pomodoroModes, PomodoroMode, pomodoroComponetsStyle } from '@/app/lib/utils/pomdoro'
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import CloseIcon from '@mui/icons-material/Close';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box/Box'
-import { Divider } from '@mui/material'
 import ReplayIcon from '@mui/icons-material/Replay';
+import GenericDialog from '@/app/lib/components/generic/GenericDialog'
 
-type TimersAmount = {
-  focus: number,
-  shortBreak: number,
-  longBreak: number
+type Settings = {
+  focusMinutes: number,
+  shortBreakMinutes: number,
+  longBreakMinutes: number
 }
 
-export default function Pomodoro() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const [isDialogOpen, setIsDialogOpen] = useState(false)
-  const [isTimerRunning, setIsTimerRunning] = useState(false)
-  const [count, setCount] = useState(25 * 60)
-  const [currentMode, setCurrentMode] = useState<PomodoroMode>(pomodoroModes.focus)
+type SettingsOptions<E> = {
+  label: string,
+  value: E,
+  onChange: (value: E) => void
+}
 
-  const [timersAmount, setTimersAmount] = useState<TimersAmount>({
-    focus: 25 * 60,
-    shortBreak: 5 * 60,
-    longBreak: 15 * 60
-  })
+export default function PomodoroTimer() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSettingsDialogOpen, setIsSettingsDialogOpen] = useState(false);
 
-  const canResetTimer = () => {
-    if (isTimerRunning) return true
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [timeInSeconds, setTimeInSeconds] = useState(25 * 60);
+  const [pomodoroMode, setPomodoroMode] = useState<PomodoroMode>(pomodoroModes.focus);
 
-    if (currentMode === pomodoroModes.focus) {
-      return count !== timersAmount.focus
-    } else if (currentMode === pomodoroModes.shortBreak) {
-      return count !== timersAmount.shortBreak
-    } else {
-      return count !== timersAmount.longBreak
+  const [settings, setSettings] = useState<Settings>({
+    focusMinutes: 25 * 60,
+    shortBreakMinutes: 5 * 60,
+    longBreakMinutes: 15 * 60,
+  });
+
+  const settingsOptions: SettingsOptions<number>[] = [
+    {
+      label: 'Focus length',
+      value: settings.focusMinutes / 60,
+      onChange: (value: number) =>
+        setSettings({
+          ...settings,
+          focusMinutes: value * 60,
+        }),
+    },
+    {
+      label: 'Short break length',
+      value: settings.shortBreakMinutes / 60,
+      onChange: (value: number) =>
+        setSettings({
+          ...settings,
+          shortBreakMinutes: value * 60,
+        }),
+    },
+    {
+      label: 'Long break length',
+      value: settings.longBreakMinutes / 60,
+      onChange: (value: number) =>
+        setSettings({
+          ...settings,
+          longBreakMinutes: value * 60,
+        }),
+    },
+  ];
+
+  const getCurrentModeTimer = () => {
+    switch (pomodoroMode) {
+      case pomodoroModes.focus:
+        return settings.focusMinutes;
+      case pomodoroModes.shortBreak:
+        return settings.shortBreakMinutes;
+      case pomodoroModes.longBreak:
+        return settings.longBreakMinutes;
+      default:
+        return 0;
     }
-  }
+  };
 
-  const handleDialogClickOpen = () => {
-    setIsDialogOpen(true)
-  }
+  const canResetTimer = () =>
+    isTimerRunning ||
+    getCurrentModeTimer() !== timeInSeconds;
 
-  const handleDialogClose = () => {
-    setIsDialogOpen(false)
+  const handleSettingsDialogClose = () => {
+    setIsSettingsDialogOpen(false);
 
-    if (isTimerRunning) return
+    if (isTimerRunning) return;
 
-    if (currentMode === pomodoroModes.focus) {
-      setCount(timersAmount.focus)
-    } else if (currentMode === pomodoroModes.shortBreak) {
-      setCount(timersAmount.shortBreak)
-    } else {
-      setCount(timersAmount.longBreak)
-    }
-  }
-
-  const changePomodoroMode = () => {
-    if (currentMode === pomodoroModes.focus) {
-      setCurrentMode(pomodoroModes.shortBreak)
-      setCount(timersAmount.shortBreak)
-    } else if (currentMode === pomodoroModes.shortBreak) {
-      setCurrentMode(pomodoroModes.longBreak)
-      setCount(timersAmount.longBreak)
-    } else {
-      setCurrentMode(pomodoroModes.focus)
-      setCount(timersAmount.focus)
-    }
-    setIsTimerRunning(false)
-  }
+    setTimeInSeconds(getCurrentModeTimer());
+  };
 
   const resetTimer = () => {
-    if (currentMode === pomodoroModes.focus) {
-      setCount(timersAmount.focus)
-    } else if (currentMode === pomodoroModes.shortBreak) {
-      setCount(timersAmount.shortBreak)
-    } else {
-      setCount(timersAmount.longBreak)
-    }
-    setIsTimerRunning(false)
-  }
+    setTimeInSeconds(getCurrentModeTimer());
+    setIsTimerRunning(false);
+  };
 
+  const changePomodoroMode = useCallback(() => {
+    switch (pomodoroMode) {
+      case pomodoroModes.focus:
+        setPomodoroMode(pomodoroModes.shortBreak);
+        setTimeInSeconds(settings.shortBreakMinutes);
+        break;
+      case pomodoroModes.shortBreak:
+        setPomodoroMode(pomodoroModes.longBreak);
+        setTimeInSeconds(settings.longBreakMinutes);
+        break;
+      case pomodoroModes.longBreak:
+        setPomodoroMode(pomodoroModes.focus);
+        setTimeInSeconds(settings.focusMinutes);
+        break;
+      default:
+        break;
+    }
+
+    setIsTimerRunning(false);
+  }, [pomodoroMode, settings, setPomodoroMode, setTimeInSeconds, setIsTimerRunning]);
+
+  
   useEffect(() => {
-    let intervalId: NodeJS.Timeout
-    if (isTimerRunning && count > 0) {
+    let intervalId: NodeJS.Timeout;
+    if (isTimerRunning && timeInSeconds === 0) {
+      changePomodoroMode();
+    }
+
+    if (isTimerRunning && timeInSeconds > 0) {
       intervalId = setInterval(() => {
-        setCount((prevCount) => prevCount - 1)
-      }, 950)
+        setTimeInSeconds((prevTime) => prevTime - 1);
+      }, 990);
 
       if (intervalId) {
-        return () => clearInterval(intervalId)
+        return () => clearInterval(intervalId);
       }
     }
-  }, [isTimerRunning, count])
+  }, [isTimerRunning, timeInSeconds, changePomodoroMode]);
 
   return (
-    <div className={`${currentMode.style}`}>
+    <div className={`${pomodoroMode.style}`}>
       <DrawerMenu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
-      <Dialog
-        open={isDialogOpen}
-        onClose={handleDialogClose}
-        aria-labelledby="responsive-dialog-title"
-        sx={{
-          '& .MuiPaper-root': {
-            borderRadius: '20px'
-          }
-        }}
-      >
-        <div
-          className={`${currentMode.style}`}
-        >
-          <DialogTitle id="responsive-dialog-title">
-            <div className='flex justify-between items-center'>
-              <strong>Settings</strong>
-              <CloseIcon fontSize='small' className='cursor-pointer' onClick={() => setIsDialogOpen(false)} />
-            </div>
-          </DialogTitle>
-          <Divider />
-          <DialogContent>
-            <div className='flex items-center justify-between '>
-              <h5 className='mr-8 text-sm'><strong>Focus length</strong></h5>
-              <Box sx={{ display: 'flex', alignItems: 'center', '& .MuiTextField-root': { m: 1, width: '7ch' } }}>
-                <TextField
-                  id="outlined-number"
-                  type="number"
-                  size='small'
-                  value={timersAmount.focus / 60}
-                  onChange={(e) => setTimersAmount({ ...timersAmount, focus: Number(e.target.value) * 60 })}
-                  slotProps={{
-                    inputLabel: {
-                      shrink: true,
-                    },
-                  }}
-                />
-              </Box>
-            </div>
-            <div className='flex items-center justify-between '>
-              <h5 className='mr-8 text-sm'><strong>Short break length</strong></h5>
-              <Box sx={{ display: 'flex', alignItems: 'center', '& .MuiTextField-root': { m: 1, width: '7ch' } }}>
-                <TextField
-                  id="outlined-number"
-                  type="number"
-                  size='small'
-                  value={timersAmount.shortBreak / 60}
-                  onChange={(e) => setTimersAmount({ ...timersAmount, shortBreak: Number(e.target.value) * 60 })}
-                  slotProps={{
-                    inputLabel: {
-                      shrink: true,
-                    },
-                  }}
-                />
-              </Box>
-            </div>
-            <div className='flex items-center justify-between '>
-              <h5 className='mr-8 text-sm'><strong>Long break length</strong></h5>
-              <Box sx={{ display: 'flex', alignItems: 'center', '& .MuiTextField-root': { m: 1, width: '7ch' } }}>
-                <TextField
-                  id="outlined-number"
-                  type="number"
-                  size='small'
-                  value={timersAmount.longBreak / 60}
-                  onChange={(e) => setTimersAmount({ ...timersAmount, longBreak: Number(e.target.value) * 60 })}
-                  slotProps={{
-                    inputLabel: {
-                      shrink: true,
-                    },
-                  }}
-                />
-              </Box>
-            </div>
-          </DialogContent>
-        </div>
-      </Dialog>
-      <ExpandMoreIcon className='absolute top-4 left-4' onClick={() => setIsMenuOpen(true)} />
+      <SettingsMenu
+        isDialogOpen={isSettingsDialogOpen}
+        onClose={handleSettingsDialogClose}
+        style={pomodoroMode.style}
+        settingsOptions={settingsOptions}
+      />
+      <ExpandMoreIcon
+        className='absolute top-4 left-4'
+        onClick={() => setIsMenuOpen(true)}
+      />
       <div className='mx-32 p-4 flex flex-col items-center h-screen align-center justify-center'>
         <div className='w-full flex justify-center'>
           <div className={`border-2 flex items-center rounded-3xl p-1 ${pomodoroComponetsStyle.mode}`}>
-            <currentMode.Icon className='mr-1 ml-1' fontSize='medium' />
-            <h5 className='text-xl mr-1 whitespace-nowrap'>{currentMode.text}</h5>
+            <pomodoroMode.Icon className='mr-1 ml-1' fontSize='medium' />
+            <h5 className='text-xl mr-1 whitespace-nowrap'>{pomodoroMode.text}</h5>
           </div>
         </div>
         <TimerDisplay
-          minutes={Math.floor(count / 60)}
-          seconds={count % 60}
+          minutes={Math.floor(timeInSeconds / 60)}
+          seconds={timeInSeconds % 60}
           isTimerRunning={isTimerRunning}
         />
         <div className='flex justify-center items-center flex-nowrap mt-4'>
           <Button
-            onClick={() => handleDialogClickOpen()}
+            onClick={() => setIsSettingsDialogOpen(true)}
             className={`${pomodoroComponetsStyle.secondButton}`}
           >
             <MoreHorizIcon fontSize='medium' />
@@ -219,14 +187,82 @@ export default function Pomodoro() {
             <Button
               disabled={!canResetTimer()}
               onClick={resetTimer}
-              className={`${canResetTimer() ? pomodoroComponetsStyle.primaryButton : pomodoroComponetsStyle.resetButton}`}
+              className={
+                canResetTimer()
+                  ? pomodoroComponetsStyle.primaryButton
+                  : pomodoroComponetsStyle.resetButton
+              }
             >
               <ReplayIcon fontSize='large' />
             </Button>
           </div>
-          <Button onClick={() => changePomodoroMode()} className={`${pomodoroComponetsStyle.secondButton}`}><FastForwardIcon fontSize='medium' /></Button>
+          <Button
+            onClick={() => changePomodoroMode()}
+            className={`${pomodoroComponetsStyle.secondButton}`}
+          >
+            <FastForwardIcon fontSize='medium' />
+          </Button>
         </div>
       </div>
     </div>
+  );
+}
+
+/**
+ * A generic settings menu dialog component.
+ *
+ * @param {string} [style] - Optional class string for styling the dialog.
+ * @param {boolean} isDialogOpen - Whether the dialog is open or not.
+ * @param {() => void} onClose - The function to be called when the dialog is closed.
+ * @param {SettingsOptions[]} settingsOptions - The options to be rendered in the dialog.
+ *
+ * @returns {JSX.Element} The dialog component.
+ */
+function SettingsMenu({
+  style,
+  isDialogOpen,
+  onClose,
+  settingsOptions,
+}: {
+  style?: string,
+  isDialogOpen: boolean,
+  onClose: () => void,
+  settingsOptions: SettingsOptions<number>[],
+}) {
+
+  return (
+    <GenericDialog
+      isOpen={isDialogOpen}
+      onClose={onClose}
+      title="Settings"
+      className={style}
+    >
+      {settingsOptions.map((option) => (
+        <div className='flex items-center justify-between' key={option.label}>
+          <h5 className='mr-8 text-sm'><strong>{option.label}</strong></h5>
+          <Box sx={{ display: 'flex', alignItems: 'center', '& .MuiTextField-root': { m: 1, width: '7ch' } }}>
+            <TextField
+              id="outlined-number"
+              type="number"
+              size='small'
+              value={option.value}
+              onChange={(e) => {
+                e.preventDefault();
+                const value = Number(e.target.value);
+                if (value < 1 || value > 99) {
+                  return;
+                }
+                option.onChange(value);
+              }}
+              slotProps={{
+                inputLabel: {
+                  shrink: true,
+                },
+              }}
+            />
+          </Box>
+        </div>
+      ))}
+    </GenericDialog >
   )
 }
