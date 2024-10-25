68605852b7413aa6d2fa59353db381ad23267acc
Balyeet1 <ricardoldias123@gmail.com>
2024-10-25 10:44:16 +0000

    Added total amount focus in a day, and changed the focus sessions list UI.

--- app/lib/components/pomodoro/CalendarDrawer.tsx
+++ app/lib/components/pomodoro/CalendarDrawer.tsx
@@ -6,6 +6,9 @@ import Drawer from '@mui/material/Drawer';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Divider from '@mui/material/Divider/Divider'
 import { pomodoroCacheApi, CookieCalendarRecords } from "@/app/lib/utils/pomdoro";
+import BoltIcon from '@mui/icons-material/Bolt'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
 
 export default function CalendarDrawer() {
@@ -48,13 +51,26 @@ export default function CalendarDrawer() {
                     <div className='mt-4' />
                     <Divider />
                     <div className="flex flex-col align-center justify-start mt-4">
-                        {currentSelectedDate && calendarRecords && calendarRecords[currentSelectedDate] && calendarRecords[currentSelectedDate].map((record, index) => (
-                            <div className="flex mx-4 flex-col align-center justify-start mb-6" key={index}>
-                                <div><strong className="text-lg mr-2">Started Time: </strong>{record.startedAt}</div>
-                                <div className="my-1"><strong className="text-lg mr-10">End Time: </strong>{record.endedAt}</div>
-                                <div><strong className="text-lg mr-12">Duration: </strong>{record.minutes} minutes</div>
-                            </div>
-                        ))}
+                        <strong className="text-2xl flex align-center justify-center mb-2">Focus Sessions</strong>
+                        <List className="shadow-lg" sx={{ overflowY: 'auto', height: '40vh' }}>
+                            {currentSelectedDate && calendarRecords && calendarRecords[currentSelectedDate] && calendarRecords[currentSelectedDate].map((record, index) => (
+                                <div>
+                                    <ListItem key={index}>
+
+                                        <BoltIcon />
+                                        <div className="flex mx-4 flex-col align-center justify-start mb-4o">
+                                            <div><strong className="text-lg mr-2">Started Time: </strong>{record.startedAt}</div>
+                                            <div className="my-1"><strong className="text-lg mr-10">End Time: </strong>{record.endedAt}</div>
+                                            <div><strong className="text-lg mr-12">Duration: </strong>{record.minutes} Minutes</div>
+                                        </div>
+                                    </ListItem>
+                                    {index !== calendarRecords[currentSelectedDate].length - 1 && <Divider />}
+                                </div>
+                            ))}
+                        </List>
+                        <div className="mx-4 mt-6 flex align-center">
+                            <strong className="mr-1">Total Focus Duration: </strong>{currentSelectedDate && calendarRecords && calendarRecords[currentSelectedDate] && calendarRecords[currentSelectedDate].reduce((acc, record) => acc + record.minutes, 0) || 0} Minutes
+                        </div>
                     </div>
                 </div>
             </Drawer >
